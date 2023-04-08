f55f5b6a31a4cabb3e74bb4c044db05ab2de1872
Md Emon Hossen <mdemong87@gmail.com>
2023-04-08 21:21:12 +0600

    final last tow

--- src/Componnent/WebinerSlider.jsx
+++ src/Componnent/WebinerSlider.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiFillPlayCircle } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
 
 export default function WebinerSlider() {
+
+    const [click, setclick] = useState(false);
+
+
     return (
         <div className='webdlideImageWrp'>
             <div className='sliderwebinerWrp'>
                 <div className='text-light playbtn'>
-                    <AiFillPlayCircle className='playIcon' />
+                    <AiFillPlayCircle onClick={() => setclick(!click)} className='playIcon' />
                 </div>
                 <h1 className='text-light text-center'>
                     Watch the video
@@ -14,6 +19,17 @@ export default function WebinerSlider() {
                     webinars
                 </h1>
             </div>
-        </div>
+
+            {
+                click && <div className='vedio'>
+                    <div className='text-light cross' onClick={() => setclick(!click)}>
+                        <ImCross />
+                    </div>
+                    <video className='veidiofram' width="100%" height="100%" controls>
+                        <source src="//www.youtube.com/embed/z0jjTU-H43M?autoplay=1&rel=0" type="video/mp4" />
+                    </video>
+                </div>
+            }
+        </div >
     )
 }
