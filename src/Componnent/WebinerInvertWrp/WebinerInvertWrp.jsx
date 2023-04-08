5efd9a0036c49aa5fd8352d1540879253b5d7853
Md Emon Hossen <mdemong87@gmail.com>
2023-04-08 21:55:03 +0600

    fianla commit

--- src/Componnent/WebinerInvertWrp/WebinerInvertWrp.jsx
+++ src/Componnent/WebinerInvertWrp/WebinerInvertWrp.jsx
@@ -1,9 +1,12 @@
-import SingleInvester from "./SingleInvester"
+import imgetwo from "../../assest/image/webinar-webinars-app.jpg";
+import imgeone from "../../assest/image/webinar-webinars-finance.jpg";
+import SingleInvester from "./SingleInvester";
+
 export default function WebinerInvertWrp() {
     return (
         <div>
-            <SingleInvester />
-            <SingleInvester />
+            <SingleInvester imge={imgeone} />
+            <SingleInvester imge={imgetwo} />
         </div>
     )
 }
